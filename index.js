750778e7da9783d7f67cd2091fb1e850ed641a61
Colleen Higgins <colleen.higgins@slalom.com>
2020-04-23 16:58:01 -0700

    doing it another way

--- index.js
+++ index.js
@@ -19,22 +19,7 @@ function main() {
   $(main);
 
   $(document).ready(function() {
-    
-    chrome.webRequest.onHeadersReceived.addListener(info => {
-    const headers = info.responseHeaders; // original headers
-    for (let i=headers.length-1; i>=0; --i) {
-        let header = headers[i].name.toLowerCase();
-        if (header === "content-security-policy") { // csp header is found
-            // modifying frame-ancestors; this implies that the directive is already present
-            headers[i].value = headers[i].value.replace("frame-ancestors", "frame-ancestors https://coggins87.github.io/portfolio-colleen/");
-        }
-    }
-    // return modified headers
-    return {responseHeaders: headers};
-}, {
-    urls: [ "<all_urls>" ], // match all pages
-    types: [ "sub_frame" ] // for framing only
-}, ["blocking", "responseHeaders"]);
+
 
     var win = $('#iframe').get(0).contentWindow;
 
@@ -52,6 +37,7 @@ function main() {
 
 function handleMessage(event){
   console.log(event.origin);
+  console.log(event.data)
   const { action, key, value } = event.data;
   console.log("Message handler", action, key);
   // if (!domains.includes(event.origin))
