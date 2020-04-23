fb5d4469731bd3906a000360c69b655060d82679
Colleen Higgins <colleen.higgins@slalom.com>
2020-04-23 16:46:02 -0700

    fe

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ function main() {
   $(main);
 
   $(document).ready(function() {
+    
+    chrome.webRequest.onHeadersReceived.addListener(info => {
+    const headers = info.responseHeaders; // original headers
+    for (let i=headers.length-1; i>=0; --i) {
+        let header = headers[i].name.toLowerCase();
+        if (header === "content-security-policy") { // csp header is found
+            // modifying frame-ancestors; this implies that the directive is already present
+            headers[i].value = headers[i].value.replace("frame-ancestors", "frame-ancestors https://yourpage.com/");
+        }
+    }
+    // return modified headers
+    return {responseHeaders: headers};
+}, {
+    urls: [ "<all_urls>" ], // match all pages
+    types: [ "sub_frame" ] // for framing only
+}, ["blocking", "responseHeaders"]);
+
     var win = $('#iframe').get(0).contentWindow;
 
   
