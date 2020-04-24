e9db0f512ce2e26b6c79991c71898008b31fb70e
Colleen Higgins <colleen.higgins@slalom.com>
2020-04-24 15:12:16 -0700

    hm

--- index.js
+++ index.js
@@ -21,34 +21,37 @@ $( window ).on( "load", function() {
  
 console.log('colleen', document.getElementById("iframe"))
   document.getElementById("iframe").contentWindow.postMessage('hi', '*')
+
+  window.addEventListener("message", function(event){
+    console.log('colleen', event.origin);
+    const { action, key, value } = event.data;
+    console.log("Message handler", event.data);
+    // if (!domains.includes(event.origin))
+    //   return;
+  
+      if (action == "save") {
+        console.log('colleen', action)
+        window.localStorage.setItem(key, JSON.stringify(value));
+      } else if (action == "get") {
+        console.log('colleen', action)
+        let sentvalue = window.localStorage.getItem(key);
+        event.source.postMessage(
+          {
+            action: "returnData",
+            key,
+            sentvalue
+          },
+          "*"
+        );
+      } else if (action == "returnData") {
+        let value2 = window.localStorage.getItem(key);
+        console.log("FROM RETURN DATA", key, value2);
+      }
+  })
+  
  })
 
- window.addEventListener("message", function(event){
-  console.log('colleen', event.origin);
-  const { action, key, value } = event.data;
-  console.log("Message handler", event.data);
-  // if (!domains.includes(event.origin))
-  //   return;
-
-    if (action == "save") {
-      console.log('colleen', action)
-      window.localStorage.setItem(key, JSON.stringify(value));
-    } else if (action == "get") {
-      console.log('colleen', action)
-      let sentvalue = window.localStorage.getItem(key);
-      event.source.postMessage(
-        {
-          action: "returnData",
-          key,
-          sentvalue
-        },
-        "*"
-      );
-    } else if (action == "returnData") {
-      let value2 = window.localStorage.getItem(key);
-      console.log("FROM RETURN DATA", key, value2);
-    }
-})
+ 
 
 
 function handleMessage(event){
